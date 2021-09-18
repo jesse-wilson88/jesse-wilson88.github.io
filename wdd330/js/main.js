95e9edb1d26b3439f5b40682b2c8e649db53983d
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-09-18 14:25:19 -0600

    Stuck. Cannot get the li to have a URL tied to it.

--- wdd330/js/main.js
+++ wdd330/js/main.js
@@ -2,3 +2,34 @@
 let thedate = new Date();
 let theyear = thedate.getFullYear();
 document.querySelector("#currentyear").textContent = theyear;
+
+const assignments = [
+  {
+    label: "Week1 notes",
+    url: "week1/index.html",
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html",
+  },
+];
+
+let ul = document.createElement("ul");
+
+assignments.forEach((assignment) => {
+  const label = `${assignment.label}`;
+  const url = `${assignment.url}`;
+
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+
+  a.setAttribute = ("href", url);
+
+  a.innerHTML = url;
+  li.innerHTML = label;
+
+  li.append(a);
+  ul.append(li);
+
+  document.querySelector("div.assignments").append(ul);
+});
