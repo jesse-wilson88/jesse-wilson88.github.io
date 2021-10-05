9f15b1fc9434587c0ce15dfdb1d6d1edc685da91
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-10-05 07:55:15 -0600

    Trying to get the form example to work from the beginning of Chap 8

---
+++ wdd330/week04/search/main.js
@@ -0,0 +1,7 @@
+const input = form.elements.searchInput;
+
+input.addEventListener("focus", () => alert("focused"), false);
+
+// input.addEventListener("blur", () => alert("blurred"), false);
+
+// input.addEventListener("change", () => alert("changed"), false);
