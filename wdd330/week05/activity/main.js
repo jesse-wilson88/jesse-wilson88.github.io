fcf79686f739447b285dad844cf8e03f1f1252f6
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-10-14 22:53:00 -0600

    Added the team activity

---
+++ wdd330/week05/activity/main.js
@@ -0,0 +1,13 @@
+import Hikes from "./hikes.js";
+
+//on load grab the array and insert it into the page
+window.addEventListener("load", () => {
+  // console.log("loading....");
+  var hike = new Hikes();
+  hike.showHikeList();
+});
+
+// document.getElementsByClassName("button").addEventListener("click", () => {
+//   var hike = new Hikes();
+//   hike.toggleMenu();
+// });
