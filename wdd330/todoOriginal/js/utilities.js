b86ae00fa9e9a51adf7db10af573aa10467e9d3c
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-10-24 10:55:23 -0600

    Made sure I have a copy of my Original Code abd broken up into different JS files with the help of a friend

---
+++ wdd330/todoOriginal/js/utilities.js
@@ -0,0 +1,62 @@
+const task = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+displayTasks();
+
+// Here we use an addEventListener for the button
+button.addEventListener("click", displayTasks);
+
+// Here we use an addEventListener for the Enter key
+// The button eventListener above can be towards the top
+// but the input eventListener cannot because it has to
+// call the function.
+input.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    displayTasks();
+  }
+});
+
+const filterAll = document.getElementById("filterAll");
+const filterActive = document.getElementById("filterActive");
+const filterCompleted = document.getElementById("filterCompleted");
+
+filterAll.addEventListener("click", (event) => {
+  let value = document.querySelectorAll("li");
+
+  let data = [...value].map((item) => ({
+    timestamp: new Date(),
+    checked: item.children[0].checked,
+    task: item.children[1].innerHTML,
+  }));
+
+  console.log(data);
+});
+
+filterActive.addEventListener("click", (event) => {
+  let value = document.querySelectorAll("li");
+
+  let data = [...value]
+    .map((item) => ({
+      timestamp: new Date(),
+      checked: item.children[0].checked,
+      task: item.children[1].innerHTML,
+    }))
+    .filter((item) => !item.checked);
+
+  console.log(data);
+});
+
+filterCompleted.addEventListener("click", (event) => {
+  let value = document.querySelectorAll("li");
+
+  let data = [...value]
+    .map((item) => ({
+      timestamp: new Date(),
+      checked: item.children[0].checked,
+      task: item.children[1].innerHTML,
+    }))
+    .filter((item) => item.checked);
+
+  console.log(data);
+});
