c3c5a5b034e8f9124abfd18dcd44f23f872db865
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-10-24 23:36:48 -0600

    Update

--- wdd330/todoxxxxx/js/ls.js
+++
@@ -1,8 +0,0 @@
-function getTasks() {
-  let todos = localStorage.getItem("todos");
-  return JSON.parse(todos);
-}
-
-function saveNewTask(tasks) {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
