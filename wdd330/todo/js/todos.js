b234f8f0ae7cb4e288d6afe0af5d8608482a984f
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-10-25 08:17:24 -0600

    Added a favicon to my todo application

--- wdd330/todo/js/todos.js
+++ wdd330/todo/js/todos.js
@@ -1,42 +1,103 @@
-function displayTasks() {
-  let taskInput = input.value;
-  input.value = "";
+console.log("If this does not work clear the local cache and try again.");
 
-  if (taskInput != "") {
-    const taskItem = document.createElement("li");
-    const taskText = document.createElement("span");
-    const taskBtn = document.createElement("button");
-    const taskBox = document.createElement("input");
+let deletingtask = false;
 
-    taskItem.appendChild(taskBox);
-    taskBox.setAttribute("type", "checkbox");
-    taskItem.setAttribute("id", new Date().getTime());
-    taskItem.appendChild(taskText);
-    taskText.textContent = taskInput;
-    taskItem.appendChild(taskBtn);
-    taskBtn.textContent = "❌";
-
-    task.appendChild(taskItem);
-
-    // Here we use an addEventListener to delete a task
-    taskBtn.addEventListener("click", function () {
-      task.removeChild(taskItem);
-    });
-
-    // Here is use an addEventListener to each taskItem so
-    // they can operate seperately.
-    taskItem.addEventListener("click", statusCheckHandler);
+function addTask() {
+  if (input.value !== "") {
+    savenewlocalitem(input.value);
+    displayTasks();
+    input.value = "";
   }
+}
 
-  input.focus();
+function filterTasks(data) {
+  let dataids = data.map((i) => {
+    return i.id;
+  });
+  let items = task.getElementsByTagName("li");
+  for (const i of items) {
+    i.hidden = false;
+    if (!dataids.includes(i.id)) {
+      i.hidden = true;
+    }
+  }
+}
+
+function displayTasks() {
+  task.innerHTML = "";
+  let tasks = getlocalitems();
+
+  if (tasks !== null) {
+    for (const t of tasks) {
+      const taskItem = document.createElement("li");
+      const taskText = document.createElement("span");
+      const taskBtn = document.createElement("button");
+      const taskBox = document.createElement("input");
+
+      taskItem.appendChild(taskBox);
+      taskBox.setAttribute("type", "checkbox");
+      taskBox.setAttribute("id", t.id);
+      if (taskItem.parparentElement != null || taskItem.length == 0) {
+        console.log(tasks.length);
+        if (t.completed) {
+          taskItem.parentElement.children[1].style.textDecoration =
+            "line-through";
+        } else {
+          taskItem.parentElement.children[1].style.textDecoration = "none";
+        }
+      }
+      taskBox.checked = t.completed;
+      taskItem.setAttribute("id", t.id);
+      taskItem.appendChild(taskText);
+      taskText.textContent = t.task;
+      taskItem.appendChild(taskBtn);
+      taskBtn.textContent = "❌";
+
+      task.appendChild(taskItem);
+
+      taskBtn.addEventListener("click", function () {
+        deleteTask(taskItem);
+        updateCounter();
+      });
+
+      taskItem.addEventListener("click", statusCheckHandler);
+    }
+    updateCounter();
+  }
+}
+
+function updateCounter() {
+  let tasks = getlocalitems();
+  let completedtasks = tasks.filter((i) => !i.completed);
+  if (completedtasks.length < 1 || completedtasks.length > 1) {
+    document.querySelector(
+      "#taskTotal"
+    ).innerHTML = `${completedtasks.length} Tasks`;
+  } else {
+    document.querySelector(
+      "#taskTotal"
+    ).innerHTML = `${completedtasks.length} Task`;
+  }
+}
+
+function deleteTask(item) {
+  deletingtask = true;
+  deletelocalitem(item.id);
+  task.removeChild(item);
 }
 
 let statusCheckHandler = (event) => {
-  let text = event.target.parentElement.children[1];
-  // Check if the checkbox is checked
-  if (event.target.checked) {
-    text.style.textDecoration = "line-through";
-  } else {
-    text.style.textDecoration = "none";
+  let check = event.target.parentElement.children[1];
+  if (!deletingtask) {
+    // Check if the checkbox is checked
+    if (event.target.checked) {
+      check.style.textDecoration = "line-through";
+      completelocaltask(event.target.id);
+    } else {
+      uncompletelocaltask(event.target.id);
+      check.style.textDecoration = "none";
+    }
+    updateCounter();
   }
+  deletingtask = false;
 };
