42ed34124f6d602a272ffb431ca0d602a56b2fef
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-11-17 23:47:53 -0700

    Added some code using Fetch

---
+++ wdd330/week10/fetch/fetch.js
@@ -0,0 +1,35 @@
+url =
+  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02&latitude=43.814540699999995&longitude=-111.78491029999999&maxradiuskm=100";
+
+const getEarthquake = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  let mydata = [];
+
+  for (const i of data.features.values()) {
+    mydata.push(i.properties);
+  }
+
+  return mydata;
+};
+
+const outputdata = async () => {
+  let mydata = await getEarthquake();
+
+  for (const i of mydata) {
+    console.log(i);
+    var date = new Date(i.time);
+    formatDate =
+      date.toLocaleString("default", { month: "long" }) +
+      " " +
+      date.getUTCDate() +
+      ", " +
+      date.getUTCFullYear();
+
+    document.getElementById(
+      "quake"
+    ).innerHTML += `<li>On ${formatDate} an ${i.type} hit ${i.place} with a magnitude of ${i.mag}.</li>`;
+  }
+};
+
+outputdata();
