2792c2d41b574c5a3923158711b848080a8ea71b
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-12-05 23:14:50 -0700

    Trying to add the current tempurature to the contact info.

--- wdd330/apiproject/js/geolocation.js
+++ wdd330/apiproject/js/geolocation.js
@@ -14,42 +14,41 @@ function success(pos) {
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
 
-  // const apiURL = `//api.openweathermap.org/geo/1.0/reverse?lat=${crd.latitude}&lon=${crd.longitude}&limit={limit}&appid=${apiKey}`;
-  const apiURL = `//api.openweathermap.org/geo/1.0/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${apiKey}`;
+  const apiURL = `//api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${apiKey}&units=imperial`;
 
   fetch(apiURL)
     .then((response) => response.json())
     .then((weatherInfo) => {
       console.log(weatherInfo);
-      // Temperature variables
-      // const currently = weatherInfo.weather[0].description.replace(
-      //   /(^\w{1})|(\s+\w{1})/g,
-      //   (letter) => letter.toUpperCase()
-      // );
-      // const current_temp = Math.round(weatherInfo.main.temp);
-      // const humidity = Math.round(weatherInfo.main.humidity);
-      // const windspeed = Math.round(weatherInfo.wind.speed);
+      //Temperature variables
+      const currently = weatherInfo.weather[0].description.replace(
+        /(^\w{1})|(\s+\w{1})/g,
+        (letter) => letter.toUpperCase()
+      );
+      const current_temp = Math.round(weatherInfo.main.temp);
+      const humidity = Math.round(weatherInfo.main.humidity);
+      const windspeed = Math.round(weatherInfo.wind.speed);
 
-      // // Displays the temperature information in the Weather Summary
-      // document.getElementById("currently").innerHTML = currently;
-      // document.getElementById("current-temp").innerHTML = current_temp;
-      // document.getElementById("humidity").innerHTML = humidity;
-      // document.getElementById("windspeed").innerHTML = windspeed;
+      // Displays the temperature information in the Weather Summary
+      document.getElementById("currently").innerHTML = currently;
+      document.getElementById("current-temp").innerHTML = current_temp;
+      document.getElementById("humidity").innerHTML = humidity;
+      document.getElementById("windspeed").innerHTML = windspeed;
 
-      // // Calculates the windchill factor if there is one
-      // let windchill =
-      //   35.74 +
-      //   0.6215 * current_temp -
-      //   35.75 * Math.pow(windspeed, 0.16) +
-      //   0.4275 * current_temp * Math.pow(windspeed, 0.16);
+      // Calculates the windchill factor if there is one
+      let windchill =
+        35.74 +
+        0.6215 * current_temp -
+        35.75 * Math.pow(windspeed, 0.16) +
+        0.4275 * current_temp * Math.pow(windspeed, 0.16);
 
-      // windchill = Math.round(windchill);
+      windchill = Math.round(windchill);
 
-      // if (current_temp <= 50 && windspeed > 3) {
-      //   document.getElementById("windchill").innerHTML = `${windchill}&degF`;
-      // } else {
-      //   document.getElementById("windchill").innerHTML = "N/A";
-      // }
+      if (current_temp <= 50 && windspeed > 3) {
+        document.getElementById("windchill").innerHTML = `${windchill}&degF`;
+      } else {
+        document.getElementById("windchill").innerHTML = "N/A";
+      }
     });
 }
 
