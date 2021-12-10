f48a54b8abee4fca3354f7f4de2f925aaeee54b9
jesse-wilson88 <jesse.emmert88@gmail.com>
2021-12-10 07:13:15 -0700

    I got the birthdate to work like the phone by adding '/' between the day , month, and year but not when I edit the contact.

--- wdd330/apiproject/js/contacts.js
+++ wdd330/apiproject/js/contacts.js
@@ -20,7 +20,13 @@ function addContact() {
       .join("")
       .replace(/(\d{3})\-?(\d{3})\-?(\d{4})/, "$1-$2-$3");
   }
-  let dob = document.getElementById("dob").value.trim();
+  let dob = document
+    .getElementById("dob")
+    .value.trim()
+    .match(/\d+/g)
+    .join("")
+    .replace(/(\d{2})\/?(\d{2})\/?(\d{4})/, "$1/$2/$3");
+
   saveNewLocalContact(
     fName,
     lName,
